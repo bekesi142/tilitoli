c10ec0ad6f12c3b8ba01f873d5cce56a0bbc2ea6
bekesi142 <72067150+bekesi142@users.noreply.github.com>
2024-04-17 09:57:36 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace TiliToliTiliToli
             
 
         }
-
+        List<Button> gombok = new();
         private void keverGomb_Click(object sender, RoutedEventArgs e)
         {
 
@@ -32,9 +32,12 @@ namespace TiliToliTiliToli
 
             
             grdPanel.ColumnDefinitions.Clear();
+            grdPanel.Children.Clear();
+            gombok.Clear();
             grdPanel.RowDefinitions.Clear();
             keverGomb.Content = "Keverés";
 
+            #region Grid létrehozása
             for (int oszlopIndex = 0; oszlopIndex < 3; oszlopIndex++)
             {
                 ColumnDefinition ujOszlop = new();
@@ -46,16 +49,33 @@ namespace TiliToliTiliToli
                 RowDefinition ujSor = new();
                 grdPanel.RowDefinitions.Add(ujSor);
             }
+            #endregion 
 
-            List<Button> gombok = new();
-            int szam = 8;
+            #region Gombok létrehozása
+            int szam = 9;
             for (int i = 0; i < szam; i++)
             {
-                Button button = new Button();
-                button.Content = i + 1;
-                button.Click += Button_Click;
-                gombok.Add(button); 
+                if (i == 8)
+                {
+                    Button hiddenButton = new();
+                    hiddenButton.Visibility = Visibility.Hidden;
+                    gombok.Add(hiddenButton);
+                    break;
+                }
+
+                else
+                {
+                    Button button = new Button();
+                    button.Content = i + 1;
+                    button.Click += Button_Click;
+                    gombok.Add(button);
+                }
+                 
             }
+
+            #endregion
+
+            #region Gombok keverése és gridbe felvevése
             Keveres(gombok);
 
             for (int i = 0; i < szam; i++)
@@ -64,12 +84,28 @@ namespace TiliToliTiliToli
                 Grid.SetColumn(gombok[i], i % 3);
                 Grid.SetRow(gombok[i], i / 3);
             }
+            #endregion
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Button clickedButton = (Button)sender;
+            foreach (var gomb in gombok)
+            {
+                if (gomb.Visibility==Visibility.Hidden)
+                {
+
+                    gomb.Visibility = Visibility.Visible;
+                    gomb.Content = clickedButton.Content;
+                    gomb.Click += Button_Click;
+                }
+            }
             
+            clickedButton.Visibility = Visibility.Hidden;
+            clickedButton.Content = string.Empty;
+            
+
         }
 
         private static void Keveres<T>(List<T> lista)
